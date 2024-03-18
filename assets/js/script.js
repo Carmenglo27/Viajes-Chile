f50442bc26c4de81ec24edf4ed5acbfe9afce061
Carmen Gloria Salazar <carmenglo.salazar@gmail.com>
2024-03-18 01:45:18 -0300

    ADD: añadi smooth scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,18 @@ $("h5").click(function(){
     alert("El correo fue enviado correctamente");
      });
     });
+
+    $(document).ready(function() {
+      $("a.nav-link").on("click", function(event) {
+          if (this.hash !== "a") {
+              event.preventDefault();
+              var hash = this.hash;
+
+              $("html, body").animate({
+                  scrollTop: $(hash).offset().top
+              }, 800);
+          }
+      });
+  });
+
+  
